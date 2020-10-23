0d485da16764e96a056da2b001ae089039205a56
Alvis <chewjta@gmail.com>
2020-10-23 10:44:52 +0800

    Update firebase.prod.js

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -6,13 +6,6 @@ import "firebase/auth";
 
 // we need a config here.
 const config = {
-  apiKey: "AIzaSyCf3cej0fEtMqurfJXMDVSPWaU8gVKMlmE",
-  authDomain: "facile-20ad5.firebaseapp.com",
-  databaseURL: "https://facile-20ad5.firebaseio.com",
-  projectId: "facile-20ad5",
-  storageBucket: "facile-20ad5.appspot.com",
-  messagingSenderId: "778249842489",
-  appId: "1:778249842489:web:94353c06a0feb3e62c82e8",
 };
 
 const firebase = Firebase.initializeApp(config);
